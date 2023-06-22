4b5336951019f2b9c968861a54440282039d4939
Carlos Gomez <20b3050002@untels.edu.pe>
2023-06-22 15:34:05 -0500

    UC3_Parte02_Gomez

--- UC3_Enrique_Gomez_Ureta/UC3/views.py
+++ UC3_Enrique_Gomez_Ureta/UC3/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render, HttpResponse,redirect
 def index(request):
     return render(request,'index.html')
 # Create your views here.
+
+def index(request):
+    estudiantes = [ 'Microprocesadores', 
+                    'Ing. de Requerimientos',
+                    'Dinamica de Sistemas',
+                    'Gestion de Procesos de Negocios',
+                    'Legislacion Informatica',
+                    'Computacion de Algoritmos Gráficos',
+                    'Matematica IV']
+
+
+    return render(request,'index.html', {
+        'titulo':'Inicio',
+        'mensaje':'Proyecto Web Con DJango',
+        'estudiantes': estudiantes
+    })
